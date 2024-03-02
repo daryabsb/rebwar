44d0c323ce31e124a55c6acb26465f6670066121
Darya <daryabsb@gmail.com>
2024-03-02 10:16:27 +0300

    Fixed STATIC_DIRS in paths.py

--- src/settings/components/paths.py
+++ src/settings/components/paths.py
@@ -3,15 +3,18 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from django.conf import settings
+
 
 # Static files (CSS, JavaScript, images)
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 PROTECTED_MEDIA_ROOT = os.path.join(BASE_DIR, 'protected')
-
-print("STATIC_ROOT: ", STATIC_ROOT)
