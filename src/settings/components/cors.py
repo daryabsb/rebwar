abd2fc5e0752d39add7d4fe74664eb6fcb17125b
Darya Ibrahim <daryabsb@gmail.com>
2024-03-13 11:52:52 +0000

    added some trusted origins to cors

--- src/settings/components/cors.py
+++ src/settings/components/cors.py
@@ -3,20 +3,31 @@
 
 CORS_ALLOWED_ORIGINS = [
     "https://rebwarallaf.com",
+    "https://dr.rebwarallaf.com",
+    "https://www.rebwarallaf.com",
+    "https://dev.rebwarallaf.com",
     'http://127.0.0.1:5501',
 ]
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://\w+\dr.rebwarallaf\.com$",
+    r"^https://\w+\dr.rebwarallaf\.com$",
 ]
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['https://rebwarallaf.com', 'http://127.0.0.1:5501',]
+CSRF_TRUSTED_ORIGINS = [
+    'https://rebwarallaf.com', 
+    "https://dr.rebwarallaf.com", 
+    "https://www.rebwarallaf.com", 
+    "https://dev.rebwarallaf.com", 
+    'http://127.0.0.1:5501',]
 
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:8000",
     'http://127.0.0.1:5501',
     "https://rebwarallaf.com",
     "https://www.rebwarallaf.com",
+    "https://dev.rebwarallaf.com",
     "https://dr.rebwarallaf.com",
+
     # add other origins that should be allowed to make requests here
 ]
 
