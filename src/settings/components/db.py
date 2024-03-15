87407d81cb25479a1a6fb47c3eeae0fa37f73e68
Darya Ibrahim <daryabsb@gmail.com>
2024-03-15 14:00:56 +0300

    Modified the db component to add local db as backup

--- src/settings/components/db.py
+++ src/settings/components/db.py
@@ -2,26 +2,28 @@ from src.settings.components.env import config
 import dj_database_url
 from src.settings.components import PROJECT_PATH, BASE_DIR
 
-
 DATABASE_URL = config("DATABASE_URL", default=None)
 # DATABASE_URL = "postgis://postgres:postgres2019@database.ticqc.ae:5433/allaf_gis"
 HOST_ENV = config('HOST_ENV', default='office')
 
-
-if DATABASE_URL is not None:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True
-        )
-    }
-else:
+try:
+    if DATABASE_URL is not None:
+        DATABASES = {
+            'default': dj_database_url.config(
+                default=DATABASE_URL,
+                conn_max_age=600,
+                conn_health_checks=True
+            )
+        }
+    else:
+        raise ValueError("DATABASE_URL is not provided")
+except Exception as e:
+    print("Failed to connect to the remote database. Using local database instead.")
     DATABASE_NAME = config("DATABASE_NAME", default='allaf_db')
     DATABASE_USER = config("DATABASE_USER")
     DATABASE_PASSWORD = config("DATABASE_PASSWORD")
-    DATABASE_HOST = config("DATABASE_HOST")
-    DATABASE_PORT = config("DATABASE_PORT")
+    DATABASE_HOST = config("DATABASE_HOST")  # 127.0.0.1
+    DATABASE_PORT = config("DATABASE_PORT")  # 5433
 
     DATABASES = {
         'default': {
@@ -33,6 +35,7 @@ else:
             'PORT': DATABASE_PORT,
         }
     }
+
 
 # else:
 
